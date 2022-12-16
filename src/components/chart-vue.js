16e1545f665c9970c665b9b49e514853ffe3d811
Dmitry Reshetnikov <ru.pacman@ya.ru>
2022-12-16 16:31:08 +0300

    add initial multiline chart support

--- src/components/chart-vue.js
+++ src/components/chart-vue.js
@@ -1,27 +1,77 @@
 export default {
-    name: "ChartVue",
-    data: () => ({
-        values: [5, 6, 8, 3, 3, 4, 6, 0],
-    }),
-    computed: {
-        pathData() {
-            const rowWidth = 320 / (this.values.length - 1);
-            const rowHeight = 240 / Math.max(...this.values);
-            const coords = [];
-            this.values.forEach((value, key) => {
-                coords.push(`${key === 0 ? 'M' : 'L'} ${key * rowWidth} ${240 - value * rowHeight}`);
-            });
-            return coords.join(" ");
-        },
+  name: "ChartVue",
+  props: {
+    width: {
+      type: Number,
+      default: 640,
     },
-    render() {
-        return (
-            <svg width="320" height="240" viewBox="0 0 320 240">
-                <text x="10" y="20">
-                    SVG
-                </text>
-                <path d={this.pathData} stroke="#FF00FF" stroke-width="2" fill="transparent" vector-effect="non-scaling-stroke" />
-            </svg>
-        );
+    height: {
+      type: Number,
+      default: 480,
     },
+    colors: {
+      type: Array,
+      default: () => ["#f00", "#0f0", "#00f", "#0ff"],
+    },
+  },
+  data: () => ({
+    values: [
+      [1, 5, -5],
+      [2, 5, -5, 7],
+      [3, 5, -5, 2],
+      [4, 5, 2],
+      [6, 5, 2],
+    ],
+  }),
+  computed: {
+    pathData() {
+      const maxLength = this.getMaxLength(this.values);
+      const rowWidth = this.width / (this.values.length - 1);
+      const rowHeight = this.height / Math.max(...this.values.flat());
+
+      return maxLength.map((_, lineNumber) =>
+        this.values
+          .map(
+            (value, key) =>
+              `${key === 0 ? "M" : "L"} ${key * rowWidth} ${
+                this.height - (value[lineNumber] || 0) * rowHeight
+              }`
+          )
+          .join(" ")
+      );
+    },
+  },
+  methods: {
+    getMaxLength(val) {
+      return val.reduce(
+        (previousValue, currentValue) =>
+          previousValue.length < currentValue.length
+            ? currentValue
+            : previousValue,
+        []
+      );
+    },
+  },
+  render() {
+    return (
+      <svg
+        width={this.width}
+        height={this.height}
+        viewBox={`0 0 ${this.width} ${this.height}`}
+      >
+        <text x="10" y="20">
+          SVG
+        </text>
+        {this.pathData.map((value, index) => (
+          <path
+            d={value}
+            stroke={this.colors[index]}
+            stroke-width="2"
+            fill="transparent"
+            vector-effect="non-scaling-stroke"
+          />
+        ))}
+      </svg>
+    );
+  },
 };
